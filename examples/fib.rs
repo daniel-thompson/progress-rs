2b5ae1febc361e88747fbdd1f9ae3151a3011c7b
Daniel Thompson <daniel.thompson@linaro.org>
2022-12-08 10:00:30 +0000

    examples: fib: Adapt the example to make it a compuational challenge
    These changes mean the computational and memory storage overheads become
    significant... making the progress bar meaningful.

--- examples/fib.rs
+++ examples/fib.rs
@@ -1,30 +1,40 @@
+use num::{BigInt, One, Zero};
 use progress::*;
+use std::mem;
+use std::ops::AddAssign;
 
 #[derive(Debug)]
-pub struct Fib {
-    a: u64,
-    b: u64,
+pub struct Fib<T> {
+    a: T,
+    b: T,
     len: usize,
 }
 
-impl Fib {
+impl<T: Zero + One> Fib<T> {
     pub fn new(len: usize) -> Self {
-        Self { a: 0, b: 1, len }
+        Self {
+            a: T::zero(),
+            b: T::one(),
+            len,
+        }
     }
 }
 
-impl Iterator for Fib {
-    type Item = u64;
+pub trait AddAssignRef: Sized + AddAssign + for<'r> AddAssign<&'r Self> {}
+impl<T> AddAssignRef for T where T: AddAssign + for<'r> AddAssign<&'r T> {}
+
+impl<T: Clone + AddAssignRef> Iterator for Fib<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.len == 0 {
             return None;
         }
-
-        let res = self.a;
-        self.a = self.b;
-        self.b = res + self.a;
         self.len -= 1;
+
+        let res = self.a.clone();
+        mem::swap(&mut self.a, &mut self.b);
+        self.b += &self.a;
 
         Some(res)
     }
@@ -34,17 +44,17 @@ impl Iterator for Fib {
     }
 }
 
-impl ExactSizeIterator for Fib {}
+impl<T: Clone + AddAssignRef> ExactSizeIterator for Fib<T> {}
 
 fn main() {
-    let mut v = Vec::new();
+    let mut v = Vec::<BigInt>::new();
 
-    for n in Fib::new(27)
-        .rate_limit(std::time::Duration::from_millis(150))
-        .show_percent()
-    {
+    for n in Fib::new(102043).show_percent() {
         v.push(n);
     }
 
-    println!("{v:?}");
+    println!(
+        "The 102043th member of the fibonaci sequence is {} digits long",
+        v.last().unwrap().to_string().len()
+    );
 }
